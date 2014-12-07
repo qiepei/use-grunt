d54c561e38063fca9c04ebf6f2828485c32e024d
qiepei.qp <qiepei.qp@alibaba-inc.com>
2014-12-07 11:28:25 +0800

    opt grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,14 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        //用于清除build目录
         clean: {
             build: {
                 src: ["build"]
             }
         },
 
+        //用于合并js文件
         concat: {
             options: {
                 separator: ';'
@@ -22,6 +24,7 @@ module.exports = function(grunt) {
             }
         },
 
+        //用于压缩js文件
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -37,6 +40,24 @@ module.exports = function(grunt) {
             }
         },
 
+        //用于压缩css文件
+        cssmin: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                report:true
+            },
+            combine: {
+                files: [{
+                    expand:true,
+                    cwd:'src',
+                    ext:'-min.css',
+                    src:'**/*.css',//所有css文件
+                    dest: 'build/'//输出到此目录下
+                }]
+            }
+        },
+
+        //检查js的语法错误
         jshint: {
             files: ['gruntfile.js', 'src/**/*.js'],
             options: {
@@ -46,6 +67,27 @@ module.exports = function(grunt) {
                     console: true,
                     module: true,
                     document: true
+                }
+            }
+        },
+
+        //将src下面的html文件copy到build
+        copy: {
+            main: {
+                expand:true,
+                cwd:'src',
+                src: '**/*.html',
+                dest: 'build/'
+            }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 9090,
+                    base: 'build',
+                    hostname: '*',
+                    keepalive:true
                 }
             }
         }
@@ -58,8 +100,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     // 默认被执行的任务列表。
-    grunt.registerTask('default', ['clean:build', 'concat:dist', 'uglify:buildall', 'jshint']);
+    grunt.registerTask('default', ['clean:build', 'concat:dist', 'uglify:buildall', 'jshint', 'cssmin:combine','copy:main']);
 
 };
--- build/index/index-min.js
+++ build/index/index-min.js
@@ -1,2 +1,2 @@
-/*! my-project-test 2014-12-05 */
+/*! my-project-test 2014-12-07 */
 console.log("Im a helper");var helper=function(){return{}};console.log("hello");var aa=function(a){alert(a)};
